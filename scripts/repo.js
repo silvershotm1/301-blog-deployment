0813c665293b91724e60dda90ca926fba8a90fe8
Michael Bishop <polishthebishop@gmail.com>
2016-08-25 19:58:46 -0700

    refactored repo request & setup heroku

---
+++ scripts/repo.js
@@ -0,0 +1,27 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  // TODO: Refactor and simplify this ajax call into a get request
+  // to the proxy end point provided by server.js.
+  repos.requestRepos = function(callback) {
+    $.ajax({
+      url: 'github/users/silvershotm1/repos' +
+            '?per_page=100' +
+            '&sort=updated',
+      type: 'GET',
+      success: function(data, message, xhr) {
+        repos.all = data;
+      }
+    }).done(callback);
+  };
+
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
